9e40fce15f8f5716f92b692b0a5e6f2cad84309f
BHURA LAL <bhuralalmalhi38@gmail.com>
2025-12-28 11:49:19 +0500

    Uncomment code for displaying smoothie data

--- streamlit_APP.py
+++ streamlit_APP.py
@@ -5,8 +5,8 @@ import streamlit as st
 #
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json)
-sf_df = st.dataframe(data=smoothiefroot_response.json , use_container_width=true)
+st.text(smoothiefroot_response.json)
+#sf_df = st.dataframe(data=smoothiefroot_response.json , use_container_width=true)
 
 
 # Write directly to the app
