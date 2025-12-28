52f6edb5faf094d015591b810ba24462fdbcabd2
BHURA LAL <bhuralalmalhi38@gmail.com>
2025-12-29 00:39:31 +0500

    Update streamlit_APP.py

--- streamlit_APP.py
+++ streamlit_APP.py
@@ -56,7 +56,9 @@ if ingredients_list:
         )
        data = smootheifroot_response.json()      # dict
        sf_df = pd.DataFrame([data])   # REAL DataFrame
-       st.dataframe(sf_df, width = 'stretch')
+       # st.dataframe(sf_df, width = 'stretch')
+       st.dataframe(sf_df, width='stretch')
+
     
        st.write(INGREDIENTS_STRING) 
 
