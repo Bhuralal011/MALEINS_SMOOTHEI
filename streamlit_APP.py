f7129ee859c11bc315ef9fc11abfeca353da0af3
BHURA LAL <bhuralalmalhi38@gmail.com>
2025-12-28 12:53:01 +0500

    Uncomment dataframe display in Streamlit app

--- streamlit_APP.py
+++ streamlit_APP.py
@@ -5,9 +5,9 @@ import streamlit as st
 #
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
+#st.text(smoothiefroot_response.json)
 
-#sf_df = st.dataframe(data=smoothiefroot_response.json , use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json , use_container_width=True)
 
 
 # Write directly to the app
